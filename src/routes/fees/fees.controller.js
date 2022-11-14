038a5f0fbc6936f3233c32daeed9eb9545c4b171
sadikali karadiya <karadiyasadikali@gmail.com>
2022-11-14 15:58:19 +0530

    merged from parent

--- src/routes/fees/fees.controller.js
+++ src/routes/fees/fees.controller.js
@@ -5,11 +5,7 @@ const Fees = require("../../models/fees");
 const Academic = require("../../models/academic");
 const Classes = require("../../models/classes");
 const FeesReceipt = require("../../models/feesReceipt");
-<<<<<<< HEAD
-const {generateReceiptFunction} = require('../receipt/receipt.controller')
-=======
 const { generateReceiptFunction } = require("../receipt/receipt.controller");
->>>>>>> origin/master
 
 //---------------------------------------------------------
 //--------------- PENDING STUDENTS FEES -------------------
@@ -33,11 +29,7 @@ async function getAllPendingStudentsFees(req, res, next) {
           "-_id class_name medium stream batch_start_year batch_end_year is_active",
         match: {
           is_active: 1,
-<<<<<<< HEAD
-          is_primary
-=======
           is_primary,
->>>>>>> origin/master
         },
       })
       .populate({
@@ -55,11 +47,7 @@ async function getAllPendingStudentsFees(req, res, next) {
     if (!pending_students[0]) {
       return res.status(200).json({
         success: false,
-<<<<<<< HEAD
-        message: 'No students with pending fees'
-=======
         message: "No students with pending fees",
->>>>>>> origin/master
       });
     }
 
@@ -131,13 +119,8 @@ async function getAllPendingStudentsFees(req, res, next) {
 //---------------------------------------------------------
 //------- PARTICULAR STUDENTS ALL ACADEMIC DETAILS --------
 //---------------------------------------------------------
-<<<<<<< HEAD
-async function studentAllAcademicDetails(req, res, next){
-  try{
-=======
 async function studentAllAcademicDetails(req, res, next) {
   try {
->>>>>>> origin/master
     const student_id = req.params.student_id;
 
     const student_details = await Student.findOne({ student_id });
@@ -145,15 +128,6 @@ async function studentAllAcademicDetails(req, res, next) {
     const academic_details = await Academic.find({
       student_id: student_details._id,
     })
-<<<<<<< HEAD
-    .populate({
-      path: "class_id"
-    })
-    .populate({
-      path: "fees_id"
-    })
-    .sort({date: -1});
-=======
       .populate({
         path: "class_id",
       })
@@ -161,18 +135,12 @@ async function studentAllAcademicDetails(req, res, next) {
         path: "fees_id",
       })
       .sort({ date: -1 });
->>>>>>> origin/master
 
     res.status(200).json({
       success: true,
       academic_details,
     });
-<<<<<<< HEAD
-  }
-  catch(error){
-=======
   } catch (error) {
->>>>>>> origin/master
     next(error);
   }
 }
@@ -181,25 +149,6 @@ async function studentAllAcademicDetails(req, res, next) {
 //------------ TRANSFER FEES TO ANOTHER STUDENT -----------
 //---------------------------------------------------------
 
-<<<<<<< HEAD
-async function transferFeesToStudent(req, res, next){
-  try{
-    const {payer_fees_id, payee_id, amount, admin_id, security_pin} = req.body;
-    const is_by_cash = 1;
-    const is_by_cheque=0;
-    const is_by_upi=0;
-    const cheque_no = -1;
-    const upi_no = '-1';
-    const discount = 0;
-
-    //---------generating Student Receipt--------
-    const result = await generateReceiptFunction(payee_id, is_by_cash, is_by_cheque, is_by_upi, amount, discount, cheque_no, upi_no, admin_id, security_pin);
-
-    if(result == false){
-      return res.status(200).json({
-          success: false,
-          message: 'Incorrect security pin'
-=======
 async function transferFeesToStudent(req, res, next) {
   try {
     const { payer_fees_id, payee_id, amount, admin_id, security_pin } =
@@ -229,24 +178,10 @@ async function transferFeesToStudent(req, res, next) {
       return res.status(200).json({
         success: false,
         message: "Incorrect security pin",
->>>>>>> origin/master
       });
     }
 
     //-------Deduct amount from payers fees-----
-<<<<<<< HEAD
-    const fees_details = await Fees.findByIdAndUpdate(payer_fees_id, {
-      $inc: { pending_amount: amount }
-    }, {returnOriginal: false, new: true})
-
-    res.status(200).json({
-      success: true,
-      message:'Fees successfully transferred', 
-      fees_details
-    });
-  }
-  catch(error){
-=======
     const fees_details = await Fees.findByIdAndUpdate(
       payer_fees_id,
       {
@@ -261,15 +196,10 @@ async function transferFeesToStudent(req, res, next) {
       fees_details,
     });
   } catch (error) {
->>>>>>> origin/master
     next(error);
   }
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 //---------------------------------------------------------
 //--------------- STUDENT FEES HISTORY --------------------
 //---------------------------------------------------------
@@ -278,16 +208,6 @@ async function studentFeesHistory(req, res, next) {
   try {
     const academic_id = req.params.academic_id;
 
-<<<<<<< HEAD
-    const academic_details = await Academic.findById(academic_id)
-
-    const fees_details = await Fees.findById(academic_details.fees_id);
-
-    const all_receipts = await FeesReceipt.find({fees_id: fees_details._id})
-    .populate('admin_id')
-    .populate('transaction_id');
-
-=======
     const academic_details = await Academic.findById(academic_id);
 
     const fees_details = await Fees.findById(academic_details.fees_id);
@@ -295,7 +215,6 @@ async function studentFeesHistory(req, res, next) {
     const all_receipts = await FeesReceipt.find({ fees_id: fees_details._id })
       .populate("admin_id")
       .populate("transaction_id");
->>>>>>> origin/master
 
     res.status(200).json({
       success: true,
@@ -311,9 +230,5 @@ module.exports = {
   // getStudentFeesDetails,
   studentFeesHistory,
   studentAllAcademicDetails,
-<<<<<<< HEAD
-  transferFeesToStudent
-=======
   transferFeesToStudent,
->>>>>>> origin/master
 };
