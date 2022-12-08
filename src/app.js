2e4c80d616e0f7a837c4b8be06421fbf8a864d65
Deepak Prajapati <prajapatideepak073@gmail.com>
2022-12-08 11:56:15 +0530

    merge frotnend into backend :tada:

--- src/app.js
+++ src/app.js
@@ -9,21 +9,23 @@ const reportRouter = require("./routes/report/report.route");
 const classesRouter = require("./routes/classes/classes.routes");
 const cors = require("cors");
 const mailRouter = require("./routes/mail/mail.route");
-
+const path = require("path");
 const app = express();
 
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
-  );
-  
+);
+
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "..", "client")));
+
 app.use(express.static("public/images"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/students", studentRouter,);
+app.use("/students", studentRouter);
 app.use("/fees", feesRouter);
 app.use("/admin", adminRouter);
 app.use("/classes", classesRouter);
@@ -41,9 +43,11 @@ app.use((req, res, next) => {
   const err = new Error("Page Not Found");
   err.status = 404;
   next(err);
-})
+});
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({success: false, message: err.message});
-})
-
+  res.status(err.status || 500).json({ success: false, message: err.message });
+});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
